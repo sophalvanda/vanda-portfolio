4b8d1f30257932f9f0ac7897c077284f4674f55d
sophalvanda <vandasophal36@gmail.com>
2026-03-14 00:43:26 +0700

    Fix missing Vuetify dependency

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }),
+  ],
   server: {
     fs: {
       // Allow serving files from one level up to the project root
