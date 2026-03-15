36e4441621312ce77b95e909d4520e2e56c06db3
sophalvanda <vandasophal36@gmail.com>
2026-03-15 08:56:48 +0700

    update config

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // It will use the variable from .env, or default to localhost if not found
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api/v1', 
+    baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/v1', 
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
