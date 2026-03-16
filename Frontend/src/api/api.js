ff2b5ace2ca95e3be8a585df06bf90aa373c46c8
sophalvanda <vandasophal36@gmail.com>
2026-03-16 23:04:59 +0700

    update config

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   // This will look for the IP during the CI/CD build process
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://13.239.31.154/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'http://13.239.31.154/api/v1',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
