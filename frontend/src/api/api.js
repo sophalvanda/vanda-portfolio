270d16ddf0f652950219dc38e90de3e1ce43222f
sophalvanda <vandasophal36@gmail.com>
2026-03-25 00:11:43 +0700

    reconfig env

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({
-    // This line dynamically picks the URL based on which .env is active
+// Add 'export' here
+export const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL
 });
-
-export default api;
