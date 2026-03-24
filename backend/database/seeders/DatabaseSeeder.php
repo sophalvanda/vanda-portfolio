b58c03f02bbbad2105c5014df15ffa99eb3226fd
sophalvanda <vandasophal36@gmail.com>
2026-03-25 00:23:39 +0700

    create seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Certification;
+use App\Models\Project;
+use App\Models\Skill;
+use App\Models\Tool;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,5 +17,9 @@ class DatabaseSeeder extends Seeder
     {
         // Call your specific seeders here, for example:
         $this->call(ExperienceSeeder::class);
+        $this->call(ProjectSeeder::class);
+        $this->call(CertificationSeeder::class);
+        $this->call(SkillSeeder::class);
+        $this->call(ToolSeeder::class);
     }
 }
